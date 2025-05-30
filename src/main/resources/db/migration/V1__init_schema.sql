ddb0e977ff2d47024f6bac53a9d9a44762f792bf
unknown <trufanovwork@gmail.com>
2025-05-30 16:27:00 +0300

    init schema for entities

---
+++ src/main/resources/db/migration/V1__init_schema.sql
@@ -0,0 +1,49 @@
+CREATE TABLE shelters (
+    id BIGSERIAL PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    city VARCHAR(50) NOT NULL,
+    number VARCHAR(20),
+    email VARCHAR(100)
+);
+
+CREATE TABLE pets (
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    description TEXT,
+    animal_kind VARCHAR(50),
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    shelter_id BIGINT NOT NULL REFERENCES shelters(id)
+);
+
+CREATE TABLE users (
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE pet_photos (
+    pet_id BIGINT NOT NULL REFERENCES pets(id),
+    photo_url VARCHAR(255) NOT NULL,
+    PRIMARY KEY (pet_id, photo_url)
+);
+
+CREATE TABLE user_favorite_pets (
+    user_id BIGINT NOT NULL REFERENCES users(id),
+    pet_id BIGINT NOT NULL REFERENCES pets(id),
+    PRIMARY KEY (user_id, pet_id)
+)
+
+CREATE TABLE comments (
+    id BIGSERIAL PRIMARY KEY,
+    content VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    author_id BIGINT REFERENCES users(id),
+    shelter_id BIGINT REFERENCES shelters(id),
+    pet_id BIGINT REFERENCES pets(id),
+    CHECK (
+        (author_id IS NOT NULL AND shelter_id IS NULL) OR
+        (shelter_id IS NOT NULL AND author_id IS NULL)
+    )
+);
