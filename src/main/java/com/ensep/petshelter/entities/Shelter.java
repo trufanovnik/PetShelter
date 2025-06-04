eb1a5a534fcdfc15d1bfa3eb2a8ed7ffef9e7f16
unknown <trufanovwork@gmail.com>
2025-06-04 11:16:33 +0300

    helper methods for add and remove comment for shelter

--- src/main/java/com/ensep/petshelter/entities/Shelter.java
+++ src/main/java/com/ensep/petshelter/entities/Shelter.java
@@ -1,5 +1,6 @@
 package com.ensep.petshelter.entities;
 
+import com.ensep.petshelter.repositories.CommentRepository;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -33,4 +34,14 @@ public class Shelter {
 
     @OneToMany(mappedBy = "shelter", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Comment> comments = new ArrayList<>();
+
+    public void addComment(Comment comment){
+        comments.add(comment);
+        comment.setShelter(this);
+    }
+
+    public void removeComment(Comment comment){
+        comments.remove(comment);
+        comment.setShelter(null);
+    }
 }
