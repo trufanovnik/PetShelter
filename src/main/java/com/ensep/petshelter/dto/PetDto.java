79e4572fa3956ac71114ffb1ba8f1db847da246f
unknown <trufanovwork@gmail.com>
2025-06-13 18:05:09 +0300

    create dto for Pet entity

---
+++ src/main/java/com/ensep/petshelter/dto/PetDto.java
@@ -0,0 +1,12 @@
+package com.ensep.petshelter.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PetDto {
+    private Long id;
+    private String name;
+    private String description;
+}
