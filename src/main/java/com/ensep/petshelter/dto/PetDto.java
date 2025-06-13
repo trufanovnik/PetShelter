ba002a80a2fccd520d2e37c55c6b33d1c2ab7662
unknown <trufanovwork@gmail.com>
2025-06-13 21:02:52 +0300

    add animal kind for Pet dto

--- src/main/java/com/ensep/petshelter/dto/PetDto.java
+++ src/main/java/com/ensep/petshelter/dto/PetDto.java
@@ -1,12 +1,12 @@
 package com.ensep.petshelter.dto;
 
-import lombok.Getter;
-import lombok.Setter;
+import com.ensep.petshelter.entities.AnimalKind;
+import lombok.Data;
 
-@Getter
-@Setter
+@Data
 public class PetDto {
     private Long id;
     private String name;
     private String description;
+    private AnimalKind animalKind;
 }
