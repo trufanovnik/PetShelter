5df06a97d1aa88c8dd5aba0b8662fbf2ee11952a
unknown <trufanovwork@gmail.com>
2025-06-16 12:35:16 +0300

    rename user to userEntity for future security

--- src/test/java/com/ensep/petshelter/repository/pet/PetRepositoryTest.java
+++ src/test/java/com/ensep/petshelter/repository/pet/PetRepositoryTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -35,7 +34,7 @@ public class PetRepositoryTest {
     private LikeRepository likeRepository;
 
     private Pet pet;
-    private User user;
+    private UserEntity userEntity;
     private Shelter shelter;
 
     @BeforeEach
@@ -45,12 +44,12 @@ public class PetRepositoryTest {
         shelter.setCity("Minsk");
         shelter = shelterRepository.save(shelter);
 
-        user = new User();
-        user.setName("Igor");
-        user.setEmail("email@email.com");
-        user.setUsername("igor123");
-        user.setPassword("123");
-        user = userRepository.save(user);
+        userEntity = new UserEntity();
+        userEntity.setName("Igor");
+        userEntity.setEmail("email@email.com");
+        userEntity.setUsername("igor123");
+        userEntity.setPassword("123");
+        userEntity = userRepository.save(userEntity);
 
         pet = new Pet();
         pet.setName("Zodiac");
@@ -67,11 +66,11 @@ public class PetRepositoryTest {
     @Test
     public void testDeletingCommentUpdatesOwnerCollections() {
         pet = petRepository.findById(pet.getId()).orElseThrow();
-        user = userRepository.findById(user.getId()).orElseThrow();
+        userEntity = userRepository.findById(userEntity.getId()).orElseThrow();
 
         Comment comment = new Comment();
         comment.setContent("Lovely pet!");
-        comment.setAuthor(user);
+        comment.setAuthor(userEntity);
         comment.setPet(pet);
         commentRepository.save(comment);
 
@@ -85,9 +84,9 @@ public class PetRepositoryTest {
         entityManager.clear();
 
         Pet updatedPet = petRepository.findById(pet.getId()).orElseThrow();
-        User updatedUser = userRepository.findById(user.getId()).orElseThrow();
+        UserEntity updatedUserEntity = userRepository.findById(userEntity.getId()).orElseThrow();
 
         assertThat(updatedPet.getComments()).isEmpty();
-        assertThat(updatedUser.getComments()).isEmpty();
+        assertThat(updatedUserEntity.getComments()).isEmpty();
     }
 }
