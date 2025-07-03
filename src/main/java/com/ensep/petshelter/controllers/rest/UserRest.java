6ca64bb7c9fd48c0f2f789999a6a47f53b2404e5
unknown <trufanovwork@gmail.com>
2025-07-03 15:33:59 +0300

    generic type for User rest controller

--- src/main/java/com/ensep/petshelter/controllers/rest/UserRest.java
+++ src/main/java/com/ensep/petshelter/controllers/rest/UserRest.java
@@ -1,5 +1,6 @@
 package com.ensep.petshelter.controllers.rest;
 
+import com.ensep.petshelter.dto.user.UserEntityDTO;
 import com.ensep.petshelter.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -15,17 +16,17 @@ public class UserRest {
     private final UserService userService;
 
     @GetMapping("/{id}")
-    public ResponseEntity<?> findById(@PathVariable Long id){
+    public ResponseEntity<UserEntityDTO> findById(@PathVariable Long id){
         return ResponseEntity.ok(userService.findById(id));
     }
 
     @PatchMapping("/{id}")
-    public ResponseEntity<?> updateUser(@PathVariable Long id, @RequestBody Map<String, Object> updates){
+    public ResponseEntity<UserEntityDTO> updateUser(@PathVariable Long id, @RequestBody Map<String, Object> updates){
         return ResponseEntity.ok(userService.updateUser(id, updates));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> deleteUser(@PathVariable Long id){
+    public ResponseEntity<String> deleteUser(@PathVariable Long id){
         userService.removeUser(id);
         return ResponseEntity.noContent().build();
     }
