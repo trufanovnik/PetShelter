45c04512f9f593c813f71d90c1f2d773aaf65be1
unknown <trufanovwork@gmail.com>
2025-07-07 15:30:41 +0300

    findByIdOrThrow exception for comment

--- src/main/java/com/ensep/petshelter/repositories/CommentRepository.java
+++ src/main/java/com/ensep/petshelter/repositories/CommentRepository.java
@@ -1,6 +1,7 @@
 package com.ensep.petshelter.repositories;
 
 import com.ensep.petshelter.entities.Comment;
+import com.ensep.petshelter.exceptions.ResourceNotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,4 +10,11 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findByAuthorId(Long author_id);
     List<Comment> findByShelterId(Long shelter_id);
     List<Comment> findByPetId(Long pet_id);
+
+    default Comment findByIdOrThrow(Long id) {
+        return findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(
+                        "Комментарий с id " + id + " не найден."
+                ));
+    }
 }
