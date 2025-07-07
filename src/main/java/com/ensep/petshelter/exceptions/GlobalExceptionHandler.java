3e4672bab407091b4df0b34cd08038dbd6d0e560
unknown <trufanovwork@gmail.com>
2025-07-07 15:28:49 +0300

    create resource not found exception with NOT FOUND handler

---
+++ src/main/java/com/ensep/petshelter/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.ensep.petshelter.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> handleResourceNotFound(ResourceNotFoundException ex){
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(ex.getMessage());
+    }
+}
