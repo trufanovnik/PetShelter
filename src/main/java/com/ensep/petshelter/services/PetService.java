67b4aec60587411ceca5ab71b92cb57a9b2cac91
unknown <trufanovwork@gmail.com>
2025-07-07 15:30:11 +0300

    findByIdOrThrow exception for pet

--- src/main/java/com/ensep/petshelter/services/PetService.java
+++ src/main/java/com/ensep/petshelter/services/PetService.java
@@ -4,11 +4,11 @@ import com.ensep.petshelter.dto.pet.PetDTO;
 import com.ensep.petshelter.dto.pet.PetDetailsDTO;
 import com.ensep.petshelter.entities.Comment;
 import com.ensep.petshelter.entities.Pet;
+import com.ensep.petshelter.exceptions.ResourceNotFoundException;
 import com.ensep.petshelter.mapper.PetDtoMapper;
 import com.ensep.petshelter.repositories.CommentRepository;
 import com.ensep.petshelter.repositories.PetRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -32,13 +32,14 @@ public class PetService {
 
     @Transactional(readOnly = true)
     public PetDetailsDTO findById(Long id){
-        Pet pet = petRepository.findById(id).orElse(null);
+        Pet pet = petRepository.findByIdOrThrow(id);
         return petDtoMapper.toPetDetailsDTO(pet);
     }
 
     @Transactional
     public PetDetailsDTO addComment(Long id, Comment comment){
-        Pet pet = petRepository.findById(id).orElse(null);
+        Pet pet = petRepository.findByIdOrThrow(id);
+
         Comment newComment = new Comment();
         newComment.setContent(comment.getContent());
         newComment.setPet(pet);
@@ -51,12 +52,13 @@ public class PetService {
 
     @Transactional
     public ResponseEntity<String> deleteCommentById(Long petId, Long commentId){
-        Pet pet = petRepository.findById(petId).orElse(null);
-        Comment comment = commentRepository.findById(commentId).orElse(null);
+        Pet pet = petRepository.findByIdOrThrow(petId);
+        Comment comment = commentRepository.findByIdOrThrow(commentId);
+
         if (!pet.getComments().contains(comment)){
-            return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                    .body("Данный комментарий не принадлежит этому питомцу.");
+            throw new ResourceNotFoundException("Комментарий не принадлежит питомцу");
         }
+
         pet.getComments().remove(comment);
         return ResponseEntity.noContent().build();
     }
