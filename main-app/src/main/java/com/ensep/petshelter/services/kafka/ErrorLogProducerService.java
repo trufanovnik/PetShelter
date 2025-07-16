65c50c768d56e335e2771274ad6278a0d2ad0a1b
unknown <trufanovwork@gmail.com>
2025-07-16 12:56:14 +0300

    kafka producer

---
+++ main-app/src/main/java/com/ensep/petshelter/services/kafka/ErrorLogProducerService.java
@@ -0,0 +1,17 @@
+package com.ensep.petshelter.services.kafka;
+
+import com.ensep.shared.dto.ErrorLogDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ErrorLogProducerService {
+
+    private final KafkaTemplate<String, ErrorLogDTO> kafkaTemplate;
+
+    public void sendError(ErrorLogDTO errorLog) {
+        kafkaTemplate.send("logs-topic", errorLog);
+    }
+}
